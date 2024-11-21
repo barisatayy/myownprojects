e6accd54da91cce7bf8dbf318c5f1d4a0d0331e8
Barış Atay <thebarisatay@gmail.com>
2024-11-22 01:00:01 +0300

    Update basamakOgrenme.java

--- projects/basamakOgrenme.java
+++ projects/basamakOgrenme.java
@@ -2,7 +2,7 @@ package schoolProject;
 
 import java.util.Scanner;
 
-public class empty {
+public class basamakOgrenme {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("pay giriniz");
